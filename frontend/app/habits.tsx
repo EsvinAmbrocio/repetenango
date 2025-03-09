f002316314413a4769290e647590709517084fad
Esvin Ambrocio <24003541@galileo.edu>
2025-03-09 12:06:58 -0600

    feat: show habit

--- frontend/app/habits.tsx
+++ frontend/app/habits.tsx
@@ -5,7 +5,7 @@ type Habits = {
 }
 export default function Habits({ habits }: Habits) {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {habits.map(habit => (
                 <HabitComponent habit={habit} key={habit._id} />
             ))}
