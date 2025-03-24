26ba657a893fa31cae75079231a4f995f6215361
Esvin Ambrocio <24003541@galileo.edu>
2025-03-23 19:00:57 -0600

    feat: login user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', async (req, res) => {
+  try{
+    const { username, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).json({message:"User registered successfully"});
+  } catch (error) {
+    res.status(500).json({ error: 'Error registering new user', description: error.toString() });
+  }
+
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username});
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password); 
+    if (!validPassword) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d'
+    });
+    res.cookie('habitToken', token, { 
+      httpOnly: false,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge:7 * (24) *60 * 6 * 1000
+    });
+    res.status(200).json({
+      message: 'User logged in successfully',
+      token
+    })
+  } catch (error) {
+    res.status(500).json({ error: 'Error logging in', description: error.toString() });
+  }
+});
 module.exports = router;
